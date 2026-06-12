f600b0a4c759105f4fcb37a5620083cf7715f2b3
varunreddy-code <adavellivarun322@gmail.com>
2026-06-12 18:05:53 +0530

    day3.py

---
+++ day3.py
@@ -0,0 +1,22 @@
+num=input("Enter a number: ")
+if num.isdigit():
+    num=int(num)
+    if num%2==0:
+        print("The number is even.")
+    else:
+        print("The number is odd.")
+else:
+    print("Invalid input. Please enter a valid number.")
+
+ A=input("Enter a number: ")
+ if A>0:
+     print("The number is positive.")       
+    elif A<0:
+        print("The number is negative.")        
+    elif:
+        print("The number is zero.")
+    elif A%2==0:
+        print("The number is even.")
+    else:        
+     print("The number is odd.")        
+    
