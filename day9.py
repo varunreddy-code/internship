260a0e5ff565949e00f0c30b98d4252f15c2f439
varunreddy-code <adavellivarun322@gmail.com>
2026-06-29 18:02:30 +0530

    Create day9.py

---
+++ day9.py
@@ -0,0 +1,19 @@
+A="vARuN"
+print(A.upper())
+print(A.lower())
+print(A.capitalize())
+B="the  pirates   of   arabian"
+print(B.title())
+print(B.strip())
+print(B.replace("pirates","parakeets"))
+print(B.split())
+print(B.find("pirates"))
+print(B.count("arabian"))
+print(B.startswith("the"))
+print(B.endswith("arabian"))
+print(B.endswith("of"))
+print(",".join("arabian"))
+
+
+
+
