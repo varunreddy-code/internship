55694f50460a88ed1f33ffb7db5981e1f53cf588
varunreddy-code <adavellivarun322@gmail.com>
2026-06-30 17:37:53 +0530

    Create day11.py

---
+++ day11.py
@@ -0,0 +1,19 @@
+class Student:
+    def __init__(self, id, name, marks):
+        self.name = name
+        self.age = age
+        self.marks = marks
+
+    def display_info(self):
+        print(f"Name: {self.name}, Age: {self.age}, Marks: {self.marks}")
+
+
+
+class Person:
+    def __init__(self, name, age, city):
+        self.name = name
+        self.age = age
+        self.city = city
+
+    def display_info(self):
+        print(f"Name: {self.name}, Age: {self.age}, City: {self.city}")
